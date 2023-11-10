5a730803b61238ce6f7339f6777693feb57265ab
Lloyd Fales <awuyunchuan@vip.qq.com>
2023-11-10 15:09:03 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -514,9 +514,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>Xiao-Ming's ChatGPT</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              基于 OpenAI & ChatGPT-Next-Web 打造的个人 AI 助手
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
